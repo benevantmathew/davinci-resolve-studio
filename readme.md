1b4a1ca8758f8ff9ec4b20721c2dc0901250606a
Benevant Mathew @build3 <benevantmathewv@gmail.com>
2025-10-01 16:42:10 +0530

    the catch is. if i am avaoiding yay. the unistall is tough.will  do a raincheck of this script

--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 # Helper scripts to install Davinci resolve studio
+
+1. sha256sum check disabled for the installer.
+idea is to manually download the zip file from "https://www.blackmagicdesign.com/support/family/davinci-resolve-and-fusion".
+And change the pkgver to latest.
+Always download the zip file to Downloads directory.
+
+2. 20.2 allowed new device activation. 20.2.1 didnot.
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+version="20.2"
+zipfile="DaVinci_Resolve_Studio_${version}_Linux.zip"
+
+# Get the directory of this script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# copy  zip file to yay cache
+if [[ ! -f "$script_dir/$zipfile" ]]; then
+    cp ~/Downloads/$zipfile "$script_dir/$zipfile"
+    echo "Copied $zipfile to yay cache."
+else
+    echo "$zipfile already exists in script directory. Skipping copy."
+fi
+
+# use "rocm-opencl-runtime"
+cd "$script_dir" || exit 1
+makepkg -is
+
+# sha256sums=('SKIP'
+#             'f17236fd68cead727c647bc31404e402922cdd491df5526f4b62364cbef9d3b8')
+# used to skip the shasum check if the package is not same
